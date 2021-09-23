61f25b450a308e3aaafdc11b7d68ea351bb93d8f
wleite <wleite@student.42sp.org.br>
2021-09-23 10:13:36 -0300

    refactor: changed execlp to execve

--- sources/pipex.c
+++ sources/pipex.c
@@ -6,13 +6,47 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/22 15:44:38 by wleite            #+#    #+#             */
-/*   Updated: 2021/09/22 19:37:38 by wleite           ###   ########.fr       */
+/*   Updated: 2021/09/23 10:12:39 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	main(int argc, char **argv)
+static void	print_args(int argc, char **argv, char **envp)
+{
+	int	i;
+
+	i = 0;
+	while (i < argc)
+	{
+		printf("\n%s argv[%d]", argv[i], i);
+		i++;
+	}
+	i = 0;
+	printf("\n");
+	while (envp[i])
+	{
+		if (strncmp("PATH", envp[i], 4) == 0)
+		{
+			//printf("\n%s envp[%d]", envp[i], i);
+			printf("\nPATH found!");
+			break ;
+		}
+		i++;
+	}
+	printf("\n");
+}
+
+// static char	*get_first_word(char *str)
+// {
+// 	int	i;
+
+// 	i = 0;
+// 	while (str[i] && str[i] != ' ')
+// 		i++;
+// }
+
+int	main(int argc, char **argv, char **envp)
 {
 	int	fd[2];
 	int	pid1;
@@ -20,6 +54,14 @@ int	main(int argc, char **argv)
 
 	if (argc >= 4)
 	{
+		char *args[3];
+
+		print_args(argc, argv, envp);
+
+		args[0] = "/usr/bin/ls";
+		args[1] = "-l";
+		args[2] = NULL;
+
 		if (pipe(fd) == -1)
 			return (1);
 
@@ -31,7 +73,7 @@ int	main(int argc, char **argv)
 			dup2(fd[1], STDOUT_FILENO);
 			close(fd[0]);
 			close(fd[1]);
-			execlp(argv[2], argv[2], NULL);
+			execve(args[0], args, NULL);
 		}
 
 		pid2 = fork();
@@ -42,8 +84,9 @@ int	main(int argc, char **argv)
 			dup2(fd[0], STDIN_FILENO);
 			close(fd[0]);
 			close(fd[1]);
-			execlp(argv[3], argv[3], NULL);
+			execve(args[0], args, NULL);
 		}
+
 		waitpid(pid1, NULL, 0);
 		waitpid(pid2, NULL, 0);
 	}
--- sources/pipex.h
+++ sources/pipex.h
@@ -6,7 +6,7 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/22 15:44:40 by wleite            #+#    #+#             */
-/*   Updated: 2021/09/22 17:28:44 by wleite           ###   ########.fr       */
+/*   Updated: 2021/09/23 00:43:31 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 # define PIPEX_H
 
 # include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
 # include <sys/types.h>
 # include <sys/wait.h>
 # include <unistd.h>
