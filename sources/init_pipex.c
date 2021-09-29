baa898709c49cc51adf62a9cef9235e54cb15fff
wleite <wleite@student.42sp.org.br>
2021-09-29 02:35:53 -0300

    refactor: changed file error message

--- sources/init_pipex.c
+++ sources/init_pipex.c
@@ -6,7 +6,7 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 07:11:39 by wleite            #+#    #+#             */
-/*   Updated: 2021/09/29 00:40:34 by wleite           ###   ########.fr       */
+/*   Updated: 2021/09/29 02:30:35 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,11 +66,11 @@ static int	open_files(t_pipex *pipex)
 {
 	pipex->file_in = open(pipex->argv[1], O_RDONLY);
 	if (pipex->file_in < 0)
-		exit_perror("ERROR (input file)", 1);
+		exit_perror(pipex->argv[1], 1);
 	pipex->file_out = open(pipex->argv[pipex->argc - 1],
 			O_CREAT | O_WRONLY | O_TRUNC, 0777);
 	if (pipex->file_out < 0)
-		exit_perror("ERROR (output file)", 1);
+		exit_perror(pipex->argv[pipex->argc - 1], 1);
 	return (0);
 }
 
