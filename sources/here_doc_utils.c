09e46767941ee8a6b64789b03a3f61bc57ea2857
wleite <wleite@student.42sp.org.br>
2021-10-05 18:07:17 -0300

    fix: checks if is an empty limiter

--- sources/here_doc_utils.c
+++ sources/here_doc_utils.c
@@ -6,7 +6,7 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/03 15:40:32 by wleite            #+#    #+#             */
-/*   Updated: 2021/10/05 16:04:57 by wleite           ###   ########.fr       */
+/*   Updated: 2021/10/05 17:48:09 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,12 @@ void	read_std_input(int *fd, char *limiter)
 {
 	char	*tmp;
 
-	(void)limiter;
 	while (1)
 	{
 		ft_putstr_fd("here_doc> ", 1);
 		tmp = ft_get_next_line(STDIN_FILENO);
-		if (ft_strncmp(tmp, limiter, ft_strlen(limiter)) == 0)
+		if (ft_strncmp(tmp, limiter, ft_strlen(limiter)) == 0
+			&& tmp[ft_strlen(limiter)] == '\n')
 		{
 			ft_free_ptr((void *)&tmp);
 			close(0);
