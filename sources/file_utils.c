7c4e6be648f21020daa262d40a2419183653bf6e
wleite <wleite@student.42sp.org.br>
2021-10-05 18:06:20 -0300

    fix: checks if there is here_doc to append to file

--- sources/file_utils.c
+++ sources/file_utils.c
@@ -6,7 +6,7 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 14:28:13 by wleite            #+#    #+#             */
-/*   Updated: 2021/10/05 14:30:57 by wleite           ###   ########.fr       */
+/*   Updated: 2021/10/05 18:02:01 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,12 @@ int	open_files(t_pipex *pipex)
 		if (pipex->file_in < 0)
 			exit_perror(pipex->argv[1], 1);
 	}
-	pipex->file_out = open(pipex->argv[pipex->argc - 1],
-			O_CREAT | O_WRONLY | O_TRUNC, 0777);
+	if (pipex->here_doc)
+		pipex->file_out = open(pipex->argv[pipex->argc - 1],
+				O_CREAT | O_WRONLY | O_APPEND, 0777);
+	else
+		pipex->file_out = open(pipex->argv[pipex->argc - 1],
+				O_CREAT | O_WRONLY | O_TRUNC, 0777);
 	if (pipex->file_out < 0)
 		exit_perror(pipex->argv[pipex->argc - 1], 1);
 	return (0);
